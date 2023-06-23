66c64b12c771369d8e9296b2dbffce4263856ebd
Jackson Freitas <jacksonzitap.mc@gmail.com>
2023-06-23 07:39:17 -0300

    creating authentication token

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,33 @@
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError');
+const { compare } = require('bcryptjs');
+const authConfig = require('../configs/auth');
+const { sign } = require('jsonwebtoken');
+
 class SessionsController{
   async create(request, response){
-    const {email, password} = request.body
+    const {email, password} = request.body;
 
-    return response.json({email, password}); //importante para garantir a parada da execução;
+    const user = await knex('users').where({ email }).first() //fazendo o select no banco
+
+    if(!user){
+      throw new AppError('E-mail e/ou senha incorreta', 401);
+    }
+
+    const passwordMatched = compare(password, user.password);
+
+    if(!passwordMatched){
+      throw new AppError('E-mail e/ou senha incorreta', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret,{
+      subject: String(user.id),
+      expiresIn
+    })
+
+
+    return response.json({user, token}); //importante para garantir a parada da execução;
   }
 }
 
