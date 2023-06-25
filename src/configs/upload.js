3644299b17907d57e438820ac261dd323e4c5797
Jackson Freitas <jacksonzitap.mc@gmail.com>
2023-06-25 09:35:15 -0300

    adding settings file for upload of images

---
+++ src/configs/upload.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const multer = require("multer");
+const crypto = require("crypto");
+
+//onde a imagem chega
+const TMP_FOLDER = path.resolve(__dirname, "..","..","tmp");
+
+//onde a imagem vai ficar
+const UPLOADS_FOLDER = path.resolve(__dirname, "uploads");
+
+
+//biblioteca para fazer o upload
+const MULTER = {
+  storage: multer.diskStorage({
+    destination: TMP_FOLDER,
+    filename(request, file, callback){
+      const fileHash = crypto.randomBytes(10).toString("Hex");
+      const fileName = `${file.originalname}-${fileHash}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
+
+
+module.exports = {
+  TMP_FOLDER,
+  UPLOADS_FOLDER,
+  MULTER,
+}
